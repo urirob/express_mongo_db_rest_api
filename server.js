cc40aed864a3260a8aef1a23fcc14658bdc34127
Tarundeep Singh <91110790+urirob@users.noreply.github.com>
2024-06-10 18:46:23 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const express=require("express");
+const errorHandler = require("./middleware/errorHandler");
+const dotenv=require("dotenv").config();
+const connectDb=require("./config/dbConnection")
+
+
+connectDb();
+
+const app=express();
+
+const port=process.env.PORT || 5000;
+
+app.use(express.json())
+
+app.use("/api/contacts",require("./routes/contactRoutes"))
+
+
+app.use(errorHandler)
+
+app.listen(port,()=>{
+    console.log(`Server running on port ${port}`)
+})
